987772e6f41ae9c7e42aa0cac706f4f9111690f5
marsett <jimenezmarset@gmail.com>
2024-12-12 20:09:41 +0100

    en el perfil se ven los datos dinámicos según el usuario que ha iniciado sesión, y al meterse en charlas se ve también dinámico, en mi perfil se ve con recursos y comentarios incluidos

--- vuegestioncharlas/src/services/PerfilService.js
+++ vuegestioncharlas/src/services/PerfilService.js
@@ -5,21 +5,41 @@ import Cookies from 'cookies-js';
 export default class PerfilService {
     getUsuarioPerfil() {
         return new Promise((resolve, reject) => {
-          const endpoint = "api/usuarios/perfil";
-          const token = Cookies.get('bearer_token');
-    
-          axios.get(Global.urlBase + endpoint, {
-            headers: {
-              Authorization: token, // Incluye el token recuperado
-            },
-          })
-            .then(response => {
-              resolve(response.data);
+            const endpoint = "api/usuarios/perfil";
+            const token = Cookies.get('bearer_token');
+
+            axios.get(Global.urlBase + endpoint, {
+                headers: {
+                    Authorization: token, // Incluye el token recuperado
+                },
             })
-            .catch(error => {
-              console.error("Error al obtener el perfil:", error.response ? error.response.data : error);
-              reject(error);
-            });
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    console.error("Error al obtener el perfil:", error.response ? error.response.data : error);
+                    reject(error);
+                });
         });
-      }
+    }
+    // Método para obtener las charlas del usuario
+    getCharlasUsuario() {
+        return new Promise((resolve, reject) => {
+            const endpoint = "api/Charlas/CharlasUser";
+            const token = Cookies.get('bearer_token'); // Obtenemos el token
+
+            axios.get(Global.urlBase + endpoint, {
+                headers: {
+                    Authorization: token, // Incluye el token en el header
+                },
+            })
+                .then(response => {
+                    resolve(response.data); // Retorna las charlas
+                })
+                .catch(error => {
+                    console.error("Error al obtener las charlas:", error.response ? error.response.data : error);
+                    reject(error);
+                });
+        });
+    }
 }
